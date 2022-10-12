a7d5ead4a8aa2983d3dbd85dcddd46f3c1ad4736
akshaycrasta201 <akshaycrasta95@gmail.com>
2022-10-12 23:27:23 +0530

    Opensea changes metadata

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x06f8eE788a94f8bD5306f50322DaBCD664A7d71E";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
-
+// const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://sophomore-whitelist-dapp-akshaycrasta201.vercel.app//api/";
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
