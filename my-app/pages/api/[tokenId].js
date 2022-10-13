37e881617c3b69aff7e6ac0cb3cbd3583134c245
akshaycrasta201 <akshaycrasta95@gmail.com>
2022-10-13 20:41:10 +0530

    metada updated

--- my-app/pages/api/[tokenId].js
+++ my-app/pages/api/[tokenId].js
@@ -2,8 +2,7 @@ export default function handler(req, res) {
     // get the tokenId from the query params
     const tokenId = req.query.tokenId;
     // As all the images are uploaded on github, we can extract the images from github directly.
-    const image_url =
-      "https://raw.githubusercontent.com/LearnWeb3DAO/NFT-Collection/main/my-app/public/cryptodevs/";
+    const image_url = "https://raw.githubusercontent.com/akshaycrasta201/LearnWeb3-Sophomore-NFT-Collection/main/my-app/public/cryptodevs/";
     // The api is sending back metadata for a Crypto Dev
     // To make our collection compatible with Opensea, we need to follow some Metadata standards
     // when sending back the response from the api
